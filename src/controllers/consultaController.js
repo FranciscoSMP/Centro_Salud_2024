1d91e0a723d82eeced5e788ffd81ad76f06d5e7a
FranciscoSMP <fmoralesp2001@gmail.com>
2024-09-30 23:33:36 -0600

    1.2

--- src/controllers/consultaController.js
+++ src/controllers/consultaController.js
@@ -35,28 +35,9 @@ exports.getConsulta = async (req, res) => {
     }
 };
 
-// exports.updateConsulta = async (req, res) => {
-//     try {
-//         await consultaModel.updateConsulta(req.body);
-//         res.redirect('/consulta/table');
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Error al actualizar la consulta');
-//     }
-// };
-
 exports.updateConsulta = async (req, res) => {
     try {
-        // Obtén la fecha del cuerpo de la solicitud
-        const fechaConsulta = new Date(req.body.Fecha_Consulta + 'T00:00:00'); // Usa T00:00:00 para asegurar que la hora es a medianoche
-
-        // Ajusta la fecha a UTC sumando 6 horas
-        const fechaUTC = new Date(fechaConsulta.getTime() + (6 * 60 * 60 * 1000)).toISOString(); // Suma 6 horas en milisegundos
-
-        await consultaModel.updateConsulta({
-            ...req.body,
-            Fecha_Consulta: fechaUTC // Usa la fecha ajustada en UTC
-        });
+        await consultaModel.updateConsulta(req.body);
         res.redirect('/consulta/table');
     } catch (error) {
         console.error(error);
@@ -64,23 +45,12 @@ exports.updateConsulta = async (req, res) => {
     }
 };
 
-// exports.getConsultaById = async (req, res) => {
-//     try {
-//         const consulta = await consultaModel.getConsultaById(req.params.id);
-//         res.render('consulta_update', { consulta });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Error al obtener la consulta');
-//     }
-// };
-
 exports.getConsultaById = async (req, res) => {
     try {
         const consulta = await consultaModel.getConsultaById(req.params.id);
-        // Cambia el formato de la fecha aquí
         const formattedConsulta = {
             ...consulta,
-            Fecha_Consulta: format(new Date(consulta.Fecha_Consulta), 'yyyy-MM-dd') // Cambia a 'yyyy-MM-dd'
+            Fecha_Consulta: format(new Date(consulta.Fecha_Consulta), 'yyyy-MM-dd')
         };
         res.render('consulta_update', { consulta: formattedConsulta });
     } catch (error) {
--- src/models/paciente.js
+++ src/models/paciente.js
@@ -20,3 +20,33 @@ exports.getPaciente = async () => {
     const result = await conSQL.request().query('SELECT * FROM Paciente');
     return result.recordset;
 };
+
+exports.updatePaciente = async ({ DPI, Primer_nombre, Segundo_nombre, Tercer_nombre, Primer_apellido, Segundo_apellido, Fecha_nacimiento, Telefono, IGSS, Genero, Id_Escolaridad, Id_Comunidad_Linguistica, Id_Profesion, Id_Disapacidad, Id_Control, Id_Pueblo, Id_Municipio, Id_Departamento }) => {
+    const query = `
+        UPDATE Paciente
+        SET Primer_nombre = '${Primer_nombre}',
+        Segundo_nombre = '${Segundo_nombre}',
+        Tercer_nombre = '${Tercer_nombre}',
+        Primer_apellido = '${Primer_apellido}',
+        Segundo_apellido = '${Segundo_apellido}',
+        Fecha_nacimiento = '${Fecha_nacimiento}',
+        Telefono = '${Telefono}',
+        IGSS = '${IGSS}',
+        Genero = '${Genero}',
+        Id_Escolaridad = ${Id_Escolaridad},
+        Id_Comunidad_Linguistica = ${Id_Comunidad_Linguistica},
+        Id_Profesion = ${Id_Profesion},
+        Id_Disapacidad = ${Id_Disapacidad},
+        Id_Control = ${Id_Control},
+        Id_Pueblo = ${Id_Pueblo},
+        Id_Municipio = ${Id_Municipio},
+        Id_Departamento = ${Id_Departamento}
+        WHERE DPI = ${DPI}`;
+    await guardarEnBaseDatos(query);
+};  
+
+exports.getPacienteById = async (id) => {
+    const conSQL = await pool.poolPromise;
+    const result = await conSQL.request().query(`SELECT * FROM Paciente WHERE DPI = ${id}`);
+    return result.recordset[0];
+};
